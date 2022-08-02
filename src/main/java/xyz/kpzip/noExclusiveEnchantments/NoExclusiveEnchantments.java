fd428f26775f0c57daddb77231a0ef2a825c6dcc
kpzip <kpzipgames@gmail.com>
2022-08-02 12:16:12 -0700

    remove unused code

--- src/main/java/xyz/kpzip/noExclusiveEnchantments/NoExclusiveEnchantments.java
+++ src/main/java/xyz/kpzip/noExclusiveEnchantments/NoExclusiveEnchantments.java
@@ -5,17 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoExclusiveEnchantments implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("noExclusiveEnchantments");
+	
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger("noexclusiveenchantments");
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 	}
 }
