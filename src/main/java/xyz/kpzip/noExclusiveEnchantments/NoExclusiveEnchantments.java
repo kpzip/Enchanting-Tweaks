9173e7378e2e914c9f8cf805372bb50c07b9f216
kpzip <kpzipgames@gmail.com>
2022-08-02 11:17:24 -0700

    refactor

--- src/main/java/xyz/kpzip/noExclusiveEnchantments/NoExclusiveEnchantments.java
+++ src/main/java/xyz/kpzip/noExclusiveEnchantments/NoExclusiveEnchantments.java
@@ -4,11 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class NoExclusiveEnchantments implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("noExclusiveEnchantments");
 
 	@Override
 	public void onInitialize() {
