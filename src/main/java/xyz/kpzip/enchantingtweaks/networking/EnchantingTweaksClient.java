28ae3a0a21e76336bb8033fb385b462549b4ba08
kpzip <kpzipgames@gmail.com>
2023-06-27 15:32:56 -0700

    remove unused imports

--- src/main/java/xyz/kpzip/enchantingtweaks/networking/EnchantingTweaksClient.java
+++ src/main/java/xyz/kpzip/enchantingtweaks/networking/EnchantingTweaksClient.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import xyz.kpzip.enchantingtweaks.EnchantingTweaks;
 import xyz.kpzip.enchantingtweaks.config.Cfgstorrer;
 
 @Environment(value=EnvType.CLIENT)
